ae159f1d262421b4eddf8651818f1dfd3fb53a21
akintola4 <akintope830@gmail.com>
2024-09-19 15:22:37 +0100

    commented out the imports for some component

--- src/Component /Auth/AuthScreen.jsx
+++ src/Component /Auth/AuthScreen.jsx
@@ -1,11 +1,11 @@
-import Login from "./Login.jsx"
-import Signup from "./SignUp.jsx"
+// import Login from "./Login.jsx"
+// import Signup from "./SignUp.jsx"
 
 export default function AuthScreen() {
   return (
     <div>
     <h3>AuthScreen</h3>
-    <Signup />
+    {/* <Signup /> */}
     </div>
     
   )
