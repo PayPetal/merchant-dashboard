b2c6abdbe212654f0a1b6d4221964ff8776ecdac
agboola <agboolanathaniel@yahoo.com>
2024-09-25 23:08:19 +0100

    change base branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import AuthScreen from "./Component /Auth/AuthScreen.jsx";
-
 function App() {
   return (
     <div className="App">
-      <AuthScreen />
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
