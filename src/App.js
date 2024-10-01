62a555f781f86f991b580a39b69c9622b6df0933
agboola <agboolanathaniel@yahoo.com>
2024-10-01 22:57:50 +0100

    feature: create svgs feature and dynamic import

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-
-import Signup from "./Component/Auth/SignUp.jsx"
-import Nav from "./Component/Auth/Nav.jsx"
+import Signup from "Component/Auth/SignUp.jsx";
+import Nav from "Component/Auth/Nav.jsx";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Login from "./Component/Auth/Login.jsx";
-import Home from "./Component/Auth/Home.jsx";
-
+import Login from "Component/Auth/Login.jsx";
+import Home from "Component/Auth/Home.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App bg-blue-600 dark:bg-blue-950  h-screen font-satoshi">
-
         <Nav />
         <div className="justify-center flex flex-col items-center">
           <Routes>
@@ -19,12 +16,8 @@ function App() {
             <Route path="/register" element={<Signup />} />
           </Routes>
         </div>
-
       </div>
-
-
     </BrowserRouter>
-
   );
 }
 
