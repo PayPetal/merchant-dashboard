8c73042bd2e083ac7eaf65da154e6c1bb62443a4
akintola4 <akintope830@gmail.com>
2024-10-09 14:59:30 +0100

    fixed the css for mobile view

--- src/layout/authlayout/index.js
+++ src/layout/authlayout/index.js
@@ -4,7 +4,7 @@ import React from "react";
 //we are going to pass anything it wraps has a child prop
 export default function Authlayout({ children }) {
   return (
-    <div className="bg-bg-img h-screen  bg-no-repeat bg-cover bg-link-a  overflow-hidden">
+    <div className="bg-bg-img h-screen overflow-scroll  bg-no-repeat bg-cover bg-link-a ">
       <Nav />
       <div className="flex justify-center">{children}</div>
     </div>
